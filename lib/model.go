256f187c7f4b5ad3ae178c6dab3f6b3957a3e6d6
sban <9830131@qq.com>
2017-12-28 16:22:38 +0800

    2017-12-28:

--- lib/model.go
+++ lib/model.go
@@ -6,10 +6,9 @@
  */
 package sim
 
-type (
+type(
 	Model struct {
-		// 如果ID没有pk tag，使用DB.ID(id)条件时，将报错：‘ID condition is error, expect 0 primarykeys, there are 1’
-		ID      int64 `xorm:"autoincr pk" json:"id"`
+		ID      int64        `xorm:"autoincr" json:"id"`
 		Updated int64 `xorm:"updated" json:"updated"` //`xorm:"updated_at"`
 		Created int64 `xorm:"created" json:"created"` //`xorm:"created_at" json:"created_at"`
 		Deleted int64 `xorm:"deleted" json:"-"`
